f6ae5399bbd44f0df2925e4ed5c4a6f3c3901fa7
Jayanth <neelajayanth557@gmail.com>
2026-04-19 20:51:13 +0530

    Add files via upload

---
+++ Array problems/EASY/Main.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,1,2};
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+    }
+}
