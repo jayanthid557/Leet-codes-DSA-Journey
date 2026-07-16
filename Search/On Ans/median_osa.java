e72c5c9f343bd3dfc76a171094966e58f5423931
Jayanth <neelajayanth557@gmail.com>
2026-07-16 21:16:58 +0530

    second version median_osa

--- Search/On Ans/median_osa.java
+++ Search/On Ans/median_osa.java
@@ -1,20 +1,37 @@
 
 public class median_osa{
     public static void main(String[] args){
-        int[] a1 = {1,3,5};
-        int[] a2 = {2,4,6};
+        int[] a = {1,3,5};
+        int[] b = {2,4,6};
 
-        int l1 = a1[0];
-        int l2 = a2[0];
-        int h1 = a1[a1.length-1];
-        int h2 = a2[a2.length-1];
+        int n = a.length + b.length;
+        int mid1 = (n-1)/2;
+        int mid2 = n/2;
 
-        int l = Math.max(l1,l2);
-        int h = Math.max(h1,h2);
+        int i = 0; int j = 0; int cnt = 0;
+        int x = 0; int y = 0;
 
-        double ans = (l+h)/2;
 
-        System.out.println(ans);
+        while(i < a.length || j < b.length){
+            int curr = 0;
 
+            if(j == b.length || (i<a.length && a[i] <= b[j])){
+                curr = a[i++];
+            }
+            else{
+                curr = b[j++];
+            }
+
+            if(cnt == mid1) x = curr;
+            if(cnt == mid2) y = curr;
+            cnt++;
+        }
+
+        if(n%2 == 0){
+            System.out.println((x+y)/2.0);
+        }
+        else{
+            System.out.println(y);
+        }
     }
 }
